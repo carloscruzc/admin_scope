e602a5c8aa5eb966e9ae4150f56d5a794bcaf3ff
CarlosC <carlos.cruz@grupolahe.com>
2022-08-04 14:53:34 -0500

    center

--- backend/App/views/worth_all.php
+++ backend/App/views/worth_all.php
@@ -1,26 +1,18 @@
-<?php echo $header; ?>
-<body class="g-sidenav-show  bg-gray-100" style="background-image: url('/assets/img/valor.jpeg'); background-repeat: no-repeat; background-position: center; background-size: 100% 100%;">
-    <!-- <aside class="bg-white-aside sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
-        <div class="sidenav-header" style="margin-bottom: 30px;">
-            <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="apple-touch-icon" sizes="76x76" href="/assets/img/favicon.png">
+        <link rel="icon" type="image/png" href="/img/favicon.png">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SCOPE</title>
+</head>  
 
-            <a class="navbar-brand m-0" href="/Principal/" target="_blank">
-                <img src="/assets/img/valor.jpeg" class="navbar-brand-img h-100" alt="main_logo">
-                <span class="ms-1 font-weight-bold"></span>
-                <p style="margin-top: 15px;"><span class="fa fa-user" style="color: #344767"></span> <?php echo $_SESSION['nombre'];?></p>
-            </a>
+<body class="g-sidenav-show  bg-gray-100" >
+    <div style="display: flex; justify-content:center;">
+    <img src="/assets/img/valor.jpeg" alt="" style="height: 100vh; ">
+    </div>
 
-
-        </div>
-        <hr class="horizontal dark mt-0">
-
-
-    </aside>
-    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
-
-    </main> -->
-
-    <!-- <center>
-    <img src="/assets/img/valor.jpeg" alt="Imagen centrada" />
-</center> -->
 </body>
+</html>

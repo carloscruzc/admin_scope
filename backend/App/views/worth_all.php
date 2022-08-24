cdc14619c62fa384912121c88344cf928701e2a5
CarlosC <carlos.cruz@grupolahe.com>
2022-08-24 14:36:01 -0500

    carlos

--- backend/App/views/worth_all.php
+++ backend/App/views/worth_all.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
+    <style>
+        .parent{
+ text-align : center;
+}
+    </style>
 <head>
 <link rel="apple-touch-icon" sizes="76x76" href="/assets/img/favicon.png">
         <link rel="icon" type="image/png" href="/img/favicon.png">
@@ -9,9 +14,9 @@
     <title>SCOPE</title>
 </head>  
 
-<body class="g-sidenav-show  bg-gray-100" >
-    <div style="display: flex; justify-content:center;">
-    <img src="/assets/img/valor.jpeg" alt="" style="height: 100vh; ">
+<body class="g-sidenav-show  bg-gray-100 parent" >
+    <div style="display: flex; justify-content:center;" >
+    <img src="/assets/img/valor.jpeg" alt="" style="height: 100vh; width: 100vw; object-fit: contain;">
     </div>
 
 </body>
